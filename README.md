04885830276064a675db42ad0fb8f8a18459e2d7
revelation <nusktecsoft@gmail.com>
2024-04-23 22:47:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# GT06 Server By Revelation A.F
+This is a GT06 GPS Tracker server implementation  written in javascript.
+It parses all messages received from the device and creates the response message, if needed.
+Eventually it will send the received information to an MQTT broker.
+
+So it acts as a server for GT06 trackers and a gateway to MQTT.
+
+## Configuration
+Run a `npm install` after you cloned it and start it via `node app.js`.
+
+You can create a `.env` file to configure the behavior off the app.
+
+The following environment variables are recognized. If not defined a default will be used.
+- GT06_SERVER_PORT=64459
+- MQTT_ROOT_TOPIC=gt06
+- MQTT_BROKER_URL=localhost
+- MQTT_BROKER_PORT=1883
+- MQTT_BROKER_PROTO=mqtt
+- MQTT_BROKER_CA=/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem
+- MQTT_BROKER_USER=user
+- MQTT_BROKER_PASSWD=passwd
+
+## MQTT
+Messages received on the TCP port will be transmitted via MQTT *MQTT_ROOT_TOPIC/IMEI/pos* if connected
+
---
+++ app.js
@@ -0,0 +1,71 @@
+require('dotenv').config();
+const Gt06 = require('./gt06');
+const Mqtt = require('mqtt');
+const net = require('net');
+const fs = require('fs');
+
+const serverPort = process.env.GT06_SERVER_PORT || 64459;
+const rootTopic = process.env.MQTT_ROOT_TOPIC || 'gt06';
+const brokerUrl = process.env.MQTT_BROKER_URL || 'localhost';
+const brokerPort = process.env.MQTT_BROKER_PORT || 1883;
+const mqttProtocol = process.env.MQTT_BROKER_PROTO || 'mqtt';
+const brokerUser = process.env.MQTT_BROKER_USER || 'user';
+const brokerPasswd = process.env.MQTT_BROKER_PASSWD || 'passwd';
+const trustedCaPath = process.env.MQTT_BROKER_CA || '';
+//const TRUSTED_CA = fs.readFileSync(trustedCaPath);
+
+// const mqttClient = Mqtt.connect(
+//     {
+//         host: brokerUrl,
+//         port: brokerPort,
+//         protocol: mqttProtocol,
+//         ca: TRUSTED_CA,
+//         username: brokerUser,
+//         password: brokerPasswd
+//     }
+// );
+//
+// mqttClient.on('error', (err) => {
+//     console.error('MQTT Error:', err);
+// });
+
+const server = net.createServer((client) => {
+    const gt06 = new Gt06();
+    console.log('client connected');
+
+    server.on('error', (err) => {
+        console.error('server error', err);
+    });
+
+    client.on('error', (err) => {
+        console.error('client error', err);
+    });
+
+    client.on('close', () => {
+        console.log('client disconnected');
+    });
+
+    client.on('data', (data) => {
+        try {
+            gt06.parse(data);
+        } catch (e) {
+            console.log('err', e);
+            return;
+        }
+        console.log(gt06);
+        if (gt06.expectsResponse) {
+            client.write(gt06.responseMsg);
+        }
+        gt06.msgBuffer.forEach(msg => {
+            // mqttClient.publish(rootTopic + '/' + gt06.imei +
+            //     '/pos', JSON.stringify(msg));
+            console.log(gt06.imei + '/pos', JSON.stringify(msg))
+        });
+        gt06.clearMsgBuffer();
+    });
+});
+
+server.listen(serverPort, () => {
+    console.log('started server on port:', serverPort);
+});
+
